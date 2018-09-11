c3967b56d52e1239e58b60c8aeb2ad46e093f055
juliageraghty <jgeraghty@solstice.com>
2018-09-11 09:29:52 -0500

    Add Address model file

---
+++ src/main/java/io/pivotal/workshop/domain/Address.java
@@ -0,0 +1,76 @@
+package io.pivotal.workshop.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table()
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String street;
+    String apartment;
+    String city;
+    String state;
+    String zip;
+    String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
